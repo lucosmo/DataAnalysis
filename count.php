3bd1e6bf693e72ca6d346613c6c9887163cf8c83
lucosmo <lukasz.kosmaczewski@gmail.com>
2014-11-04 22:26:33 +0000

    fixed few syntax errors, script counts coffees

--- count.php
+++ count.php
@@ -1,71 +1,69 @@
 <?php
-$coffee=array(
-"E"=>0;
-"ED"=>0;
-"DE"=>0;
-"DED"=>0;
-"MC"=>0;
-"MCS"=>0;
-"MCSD"=>0;
-"MCD"=>0;
-"DMC"=>0;
-"DMCS"=>0;
-"DMCD"=>0;
-"DMCSD"=>0;
-"L"=>0;
-"LS"=>0;
-"LD"=>0;
-"LSD"=>0;
-"C"=>0;
-"CS"=>0;
-"CD"=>0;
-"CSD"=>0;
-"M"=>0;
-"MS"=>0;
-"MD"=>0;
-"MSD"=>0;
-"FW"=>0;
-"FWS"=>0;
-"FWD"=>0;
-"FWSD"=>0;
-"BC"=>0;
-"BCD"=>0;);
+$coffee=array("E"=>0,"ETA"=>0,
+"ED"=>0,"EDTA"=>0,
+"DE"=>0,"DETA"=>0,
+"DED"=>0,"DEDTA"=>0,
+"MC"=>0,"MCTA"=>0,
+"MCS"=>0,"MCSTA"=>0,
+"MCSD"=>0,"MCSDTA"=>0,
+"MCD"=>0,"MCDTA"=>0,
+"DMC"=>0,"DMCTA"=>0,
+"DMCS"=>0,"DMCSTA"=>0,
+"DMCD"=>0,"DMCDTA"=>0,
+"DMCSD"=>0,"DMCSDTA"=>0,
+"L"=>0,"LTA"=>0,
+"LS"=>0,"LSTA"=>0,
+"LD"=>0,"LDTA"=>0,
+"LSD"=>0,"LSDTA"=>0,
+"C"=>0,"CTA"=>0,
+"CS"=>0,"CSTA"=>0,
+"CD"=>0,"CDTA"=>0,
+"CSD"=>0,"CSDTA"=>0,
+"M"=>0,"MTA"=>0,
+"MS"=>0,"MSTA"=>0,
+"MD"=>0,"MDTA"=>0,
+"MSD"=>0,"MSDTA"=>0,
+"FW"=>0,"FWTA"=>0,
+"FWS"=>0,"FWSTA"=>0,
+"FWD"=>0,"FWDTA"=>0,
+"FWSD"=>0,"FWSDTA"=>0,
+"BC"=>0,"BCTA"=>0,
+"BCD"=>0,"BCDTA"=>0);
 
-function readDrink($drink,&$coffee)
+function readDrink($drink)
 {
-	$n=0;
-	$signs=count($drink);
-	$drinkName="";
+	$n=1;
+	$signs=strlen($drink);
+	$drinkName='';
 	$inDrinkName=true;
+	$coffee=&$GLOBALS['coffee'];
 	for($i=0;$i<$signs;$i++)
 	{
-		
-		if(is_digit($drink[$i])) $n=$drink[$i];
+		if($i==0 && is_numeric($drink[$i])) {$n=$drink[$i];continue;}
 		else 
 		{
-			$n=1;
-			
 			if(ctype_alpha($drink[$i])&&$inDrinkName)
 			{
 				$drinkName.=$drink[$i];
 				continue;
 			}
-			elseif($drink[$i]=="/")
-			{
-				$inDrinkName=false;
-				
-			}
+			elseif($drink[$i]=="/") continue;
 		}
 		
-		
+		if(strlen($drinkName)==0) continue;
 	}
+	if(strlen($drinkName)!=0){
+		if(array_key_exists($drinkName,$coffee))
+		$coffee[$drinkName]+=$n;
+	}
+	
 }
 
 function readBuffer($buffer)
 {
 	$drink=explode(" ",$buffer);
 	$drinks=count($drink);
-	for($i=0;$i<$drinks;$i++)
+	for($i=1;$i<$drinks;$i++)
 	{
 		readDrink($drink[$i]);
 	}
@@ -73,9 +71,10 @@ function readBuffer($buffer)
 $handle=@fopen("02-11-2014.txt","r");
 if($handle)
 {
-	while($buffer=fgets($handle)!=false)
+	while(($buffer=fgets($handle))!=false)
 	{
-		if($buffer[0]&&$buffer[1]=='/') continue;
+		$buffer=substr($buffer,0,count($buffer)-3);
+		if($buffer[0]=='/'&&$buffer[1]=='/') continue;
 		readBuffer($buffer);
 		
 	}
@@ -83,5 +82,7 @@ if($handle)
 	{
 		echo "Error: unexpected fgets fail\n";
 	}
+	var_dump($coffee);
+	echo array_sum($coffee)." coffees was made<br><br>";
 	fclose($handle);
 }
