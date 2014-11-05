a9c756608aca8aaa6abdd9acbdbb172bb415184e
lucosmo <lukasz.kosmaczewski@gmail.com>
2014-11-05 21:26:53 +0000

    Using dynamic, growing on demand array of names of drinks as keys

--- count.php
+++ count.php
@@ -1,34 +1,5 @@
 <?php
-$coffee=array("E"=>0,"ETA"=>0,
-"ED"=>0,"EDTA"=>0,
-"DE"=>0,"DETA"=>0,
-"DED"=>0,"DEDTA"=>0,
-"MC"=>0,"MCTA"=>0,
-"MCS"=>0,"MCSTA"=>0,
-"MCSD"=>0,"MCSDTA"=>0,
-"MCD"=>0,"MCDTA"=>0,
-"DMC"=>0,"DMCTA"=>0,
-"DMCS"=>0,"DMCSTA"=>0,
-"DMCD"=>0,"DMCDTA"=>0,
-"DMCSD"=>0,"DMCSDTA"=>0,
-"L"=>0,"LTA"=>0,
-"LS"=>0,"LSTA"=>0,
-"LD"=>0,"LDTA"=>0,
-"LSD"=>0,"LSDTA"=>0,
-"C"=>0,"CTA"=>0,
-"CS"=>0,"CSTA"=>0,
-"CD"=>0,"CDTA"=>0,
-"CSD"=>0,"CSDTA"=>0,
-"M"=>0,"MTA"=>0,
-"MS"=>0,"MSTA"=>0,
-"MD"=>0,"MDTA"=>0,
-"MSD"=>0,"MSDTA"=>0,
-"FW"=>0,"FWTA"=>0,
-"FWS"=>0,"FWSTA"=>0,
-"FWD"=>0,"FWDTA"=>0,
-"FWSD"=>0,"FWSDTA"=>0,
-"BC"=>0,"BCTA"=>0,
-"BCD"=>0,"BCDTA"=>0);
+$coffee=(array)null;
 
 function readDrink($drink)
 {
@@ -55,6 +26,7 @@ function readDrink($drink)
 	if(strlen($drinkName)!=0){
 		if(array_key_exists($drinkName,$coffee))
 		$coffee[$drinkName]+=$n;
+		else $coffee[$drinkName]=$n;
 	}
 	
 }
