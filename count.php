b7b2a3e9712638d6e889d47afd2e87120476833c
lucosmo <lukasz.kosmaczewski@gmail.com>
2014-11-07 22:08:14 +0000

    added array of time

--- count.php
+++ count.php
@@ -1,13 +1,20 @@
 <?php
-$coffee=(array)null;
+$coffee=(array)null; // array of names of drinks as key and number of drinks as value
+$coffeeTime=(array)null; // temprorary array of time of order - just for test, tommorow wanna remove it
 $fileName="29-10-2014.txt";
-function readDrink($drink)
+
+function howMDrinksInPeriod($coffeeArray, $time1, $time2)
+{
+	
+}
+function readDrink($drink,$time)
 {
 	$n=1;
 	$signs=strlen($drink);
 	$drinkName='';
 	$inDrinkName=true;
 	$coffee=&$GLOBALS['coffee'];
+	$coffeeTime=&$GLOBALS['coffeeTime'];
 	for($i=0;$i<$signs;$i++)
 	{
 		if($i==0 && is_numeric($drink[$i])) {$n=(int)$drink[$i];continue;}
@@ -24,25 +31,31 @@ function readDrink($drink)
 		if(strlen($drinkName)==0) continue;
 	}
 	if(strlen($drinkName)!=0){
-		if(array_key_exists($drinkName,$coffee))
-		$coffee[$drinkName]+=$n;
-		else $coffee[$drinkName]=$n;
+		if(array_key_exists($drinkName,$coffee)){
+			$coffee[$drinkName]+=$n;
+			for($i=0;$i<$n;$i++)
+				$coffeeTime[$drinkName][]=$time; //bit silly idea to create new array... just for test
+		}
+		else{
+			$coffee[$drinkName]=$n;
+			$coffeeTime[$drinkName][]=$time;
+		}
 	}
 	
 }
 
-function readTime($buffer)
+function readTime($time)
 {
-	//echo strtotime($buffer)."<br>";
+	return strtotime($time);
 }
 
 function readBuffer($buffer)
 {
 	$drink=explode(" ",$buffer);
 	$drinks=count($drink);
-	readTime($drink[0]);
+	$time=readTime($drink[0]);
 	for($i=1;$i<$drinks;$i++)
-		readDrink($drink[$i]);
+		readDrink($drink[$i],$time);
 }
 $handle=@fopen($fileName,"r");
 if($handle)
@@ -60,5 +73,6 @@ if($handle)
 	}
 	var_dump($coffee);
 	echo array_sum($coffee)." coffees was made<br><br>";
+	var_dump($coffeeTime);
 	fclose($handle);
 }
