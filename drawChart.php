f6b5e5beabaf73988a11fdc2e5c94e04dc0aed9b
lucosmo <lukasz.kosmaczewski@gmail.com>
2014-11-15 21:56:06 +0000

    keep preparing class for drawing chart

--- drawChart.php
+++ drawChart.php
@@ -1,6 +1,17 @@
 <?php
 class drawChart{
 	private $data;
+	private function openFile($file)
+	{
+		$handle=@fopen($file,"r");
+		if($handle){
+			while(($buffer=fgets($handle))!=false)
+			{
+				$buffer=
+			}
+		}
+		
+	}
 	function _construct($data){
 		$this->data=$data;
 	}
